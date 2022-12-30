b72be4a7683558ca800054a79fe4d4b0e0ed8e5c
Prashant Shukla <prashant.shukla@getrightdata.com>
2022-12-30 17:17:20 +0530

    .

--- NonRepeatingCharInStr.java
+++ NonRepeatingCharInStr.java
@@ -1,3 +1,10 @@
 public class NonRepeatingCharInStr {
-    
+    public static void main(String[] args) {
+        String str = "cidbhscehcbhi";
+        for(int i=0;i<str.length();i++){
+            if(str.indexOf(str.charAt(i))==str.lastIndexOf(str.charAt(i))){
+                System.out.println(str.charAt(i));
+            }
+        }
+    }
 }
